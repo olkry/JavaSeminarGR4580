faa39ae61ff229d9ce316209f5ebfc4ed8e05fe7
olkry <adenos90@gmail.com>
2023-06-22 19:36:29 +0300

    Add seminar005

---
+++ Seminar005/src/HashMapTest/Test.java
@@ -0,0 +1,28 @@
+package HashMapTest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test {
+
+      public static HashMap<Integer, String> fill() { // Integer[] intValue,String[] values//String[] values
+            HashMap<Integer, String> outMap = new HashMap<>();
+            outMap.put(123456, "Иванов");
+            outMap.put(321456, "Васильев");
+            outMap.put(234561, "Петров");
+            outMap.put(234432, "Иванов");
+            outMap.put(654321, "Петров");
+            outMap.put(345678, "Иванов");
+            return outMap;
+      }
+
+      public static void printHashMap(HashMap<Integer, String> outMap, String searchValue) {
+
+            for (Map.Entry<Integer, String> item : outMap.entrySet()) {
+                  if (item.getValue() == searchValue) {
+                        System.out.println(item.getValue() + " - " + item.getKey());
+                  }
+            }
+      }
+
+}
---
+++ Seminar005/src/RomNumConv/Converter.java
@@ -0,0 +1,41 @@
+package RomNumConv;
+
+import java.util.List;
+
+public class Converter {
+
+      public static int romanToArab(String num) {
+            num = num.toUpperCase();
+            int result = 0;
+            List<RomanNum> romNum = RomanNum.getRevSortValue();
+            int i = 0;
+            while (num.length() > 0 && romNum.size() > i) {
+                  RomanNum symbol = romNum.get(i);
+                  if (num.startsWith(symbol.name())) {
+                        result += symbol.getValue();
+                        num = num.substring(symbol.name().length());
+                  } else {
+                        i++;
+                  }
+            }
+
+            return result;
+      }
+
+      public static String arabicToRoman(int num) {
+            List<RomanNum> romNum = RomanNum.getRevSortValue();
+            int i = 0;
+            StringBuilder sb = new StringBuilder();
+            while (num>0&&romNum.size()> i) {
+                  RomanNum symbol = romNum.get(i);
+                  if (symbol.getValue()<=num) {
+                        sb.append(symbol.name());
+                        num-=symbol.getValue();
+                  } else {
+
+                        i++;
+                  }
+            }
+            return sb.toString();
+      }
+}
